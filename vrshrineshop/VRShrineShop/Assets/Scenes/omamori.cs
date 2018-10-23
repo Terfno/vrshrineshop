b6a43a62dc98b7ee7e8be8ffe16a5c7c7847c3f3
terfno <sueda.takahito@gmail.com>
2018-10-23 09:55:09 +0900

    finish

---
+++ vrshrineshop/VRShrineShop/Assets/Scenes/omamori.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class omamori : MonoBehaviour {
+	public GameObject price;
+
+	// Use this for initialization
+	void Start () {
+		price.SetActive(false);
+	}
+	
+	void OnCollisionStay(Collision col){
+		if(col.gameObject.tag == "hand"){
+			price.SetActive(true);
+		}
+	}
+
+	void OnCollisionExit(Collision col){
+		price.SetActive(false);
+	}
+}
